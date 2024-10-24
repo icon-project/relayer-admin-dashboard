44ef39a9680b7aee41c34969de4c1741a55f6a25
Debendra Oli <debendraoli@pm.me>
2024-10-24 12:50:00 +0545

    rf: use table for chain info

--- src/components/Page/Dashboard/ChainDetails.tsx
+++ src/components/Page/Dashboard/ChainDetails.tsx
@@ -3,6 +3,7 @@
 import { ChainInfoResponse } from '@/utils/socket-fetch';
 import { notFound } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
+import { Card, CardBody, CardHeader, Table } from 'react-bootstrap';
 
 interface ChainDetailsProps {
   id: string;
@@ -36,27 +37,57 @@ const ChainDetails: React.FC<ChainDetailsProps> = ({ id }) => {
   }
 
   return (
-    <div>
-      <h1>Chain Details</h1>
-      <p>Name: {chainInfo.name}</p>
-      <p>NID: {chainInfo.nid}</p>
-      <p>Type: {chainInfo.type}</p>
-      <p>Address: {chainInfo.address}</p>
-      <p>Latest Height: {chainInfo.latestHeight}</p>
-      <p>Last CheckPoint: {chainInfo.lastCheckPoint}</p>
-      <div>
-        <h2>Contracts</h2>
-        <p>XCall: {chainInfo.contracts.xcall}</p>
-        <p>Connection: {chainInfo.contracts.connection}</p>
-      </div>
-      {chainInfo.balance && (
-        <div>
-          <h2>Balance</h2>
-          <p>Amount: {chainInfo.balance.amount}</p>
-          <p>Denom: {chainInfo.balance.denom}</p>
-        </div>
-      )}
-    </div>
+    <Card>
+      <CardHeader>
+        <h1>Chain Details</h1>
+      </CardHeader>
+      <CardBody>
+        <Table striped bordered hover>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{chainInfo.name}</td>
+            </tr>
+            <tr>
+              <td>NID</td>
+              <td>{chainInfo.nid}</td>
+            </tr>
+            <tr>
+              <td>Type</td>
+              <td>{chainInfo.type}</td>
+            </tr>
+            <tr>
+              <td>Address</td>
+              <td>{chainInfo.address}</td>
+            </tr>
+            <tr>
+              <td>Latest Height</td>
+              <td>{chainInfo.latestHeight}</td>
+            </tr>
+            <tr>
+              <td>Last CheckPoint</td>
+              <td>{chainInfo.lastCheckPoint}</td>
+            </tr>
+            <tr>
+              <td>Contracts</td>
+              <td>
+                <div>XCall: {chainInfo.contracts.xcall}</div>
+                <div>Connection: {chainInfo.contracts.connection}</div>
+              </td>
+            </tr>
+            {chainInfo.balance && (
+              <tr>
+                <td>Balance</td>
+                <td>
+                  <div>Amount: {chainInfo.balance.amount}</div>
+                  <div>Denom: {chainInfo.balance.denom}</div>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </CardBody>
+    </Card>
   );
 };
 
